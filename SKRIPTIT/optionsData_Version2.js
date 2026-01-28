54aa7ecbaf73aeee5bf2a4144736ea9407e86da5
kortsu <kortesoja.iivari@gmail.com>
2026-01-28 20:58:40 +0200

    Delete SKRIPTIT/optionsData_Version2.js

--- SKRIPTIT/optionsData_Version2.js
+++
@@ -1,6 +0,0 @@
-// Paikkakunnat ja niiden vastaavat sivut
-const paikkakuntaOptions = [
-    { name: "Valitse...", value: "" },
-    { name: "Pori", value: "valokuvaus_pori.html" },
-    { name: "Nakkila", value: "valokuvaus_nakkila.html" },
-];
