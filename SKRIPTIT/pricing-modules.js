2f5993f4eeee65a7f09d2b131a489774ad43088c
kortsu <kortesoja.iivari@gmail.com>
2026-02-02 17:30:08 +0200

    Add pricing modules for photography packages

---
+++ SKRIPTIT/pricing-modules.js
@@ -0,0 +1,31 @@
+const upperPackages = [
+    {
+        title: '3 tuntia',
+        price: '300 €',
+        desc: 'Esimerkiksi hautajaiset tai valmistujaisjuhlat'
+    },
+    {
+        title: '4–6 tuntia',
+        price: '400 €',
+        desc: 'Pidempään kestävät tilaisuudet'
+    },
+    {
+        title: '6 tuntia +',
+        price: '500 €',
+        desc: 'Esimerkiksi dokumentaarinen hääkuvaus'
+    }
+];
+
+const lowerPackages = [
+    {
+        title: 'Hääpaketti',
+        price: '700 €',
+        features: [
+            'Dokumentaarinen hääkuvaus <strong>koko häiden ajan</strong>',
+            'Elokuvallinen häävideo tärkeimmästä päivästä',
+            'Ei stressiä kuvausajasta',
+            'Eniten valokuvia'
+        ],
+        quote: 'Monet sanovat, että kuva kertoo enemmän kuin tuhat sanaa. Minä sanon, että liikkuva kuva kertoo vieläkin enemmän.'
+    }
+];
