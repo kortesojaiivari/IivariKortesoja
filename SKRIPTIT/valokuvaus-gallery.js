39e32729e3e86d67c5a73fb6ea1e9b28c4c5e2bf
kortsu <kortesoja.iivari@gmail.com>
2026-03-27 16:05:34 +0200

    Update valokuvaus-gallery.js

--- SKRIPTIT/valokuvaus-gallery.js
+++ SKRIPTIT/valokuvaus-gallery.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const basePath = window.location.pathname.includes('/Valokuvaus/') ? '../' : '';
 
     // TÄSSÄ MÄÄRITETÄÄN KAIKKI KUVAT – muutos täällä vaikuttaa kaikkiin sivuihin!
-    const smallSequence = [[40,49,1,7,13,19,25,31],[41,52,2,8,14,20,26,32],[42,51,3,9,15,21,27,33],[43,46,4,10,16,22,28,34],[44,50,5,11,17,23,29,35]];
-    const largeSequence = [[1,7,13,19,25,31,45],[2,8,14,20,26,32,43],[3,9,15,21,27,33,44],[4,10,16,22,28,34,41],[5,11,17,23,29,35,42],[6,12,18,24,30,36,40]];
+    const smallSequence = [[62,40,49,1,7,13,19,25,31],[68,41,52,2,8,14,20,26,32],[73,42,51,3,9,15,21,27,33],[54,43,46,4,10,16,22,28,34],[76,44,50,5,11,17,23,29,35]];
+    const largeSequence = [[1,7,13,62,75,19,25,31,45],[2,8,14,68,74,20,26,32,43],[3,9,15,73,65,21,27,33,44],[4,10,16,54,72,22,28,34,41],[5,11,17,76,56,23,29,35,42],[6,12,18,65,59,24,30,36,40]];
 
     // Pieni galleria (yläosa)
     const smallContainer = document.querySelector('.small-gallery');
