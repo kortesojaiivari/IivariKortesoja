12afaf02d09fb5ebe1ac5789b14fcd07cad87661
kortsu <kortesoja.iivari@gmail.com>
2026-03-27 14:55:34 +0200

    Update header.js

--- SKRIPTIT/header.js
+++ SKRIPTIT/header.js
@@ -5,7 +5,7 @@
         <a href="/index.html">Koti</a>
         <a href="/Yhteystiedot.html">Yhteystiedot</a>
         <a href="/Valokuvaus.html">Valokuvaus</a>
-        <a href="/Puolustusvoimat.html">Puolustusvoimat</a>
+        <a href="/Portfolio.html">Portfolio</a>
       </nav>
     </header>
   `;
