031ff55c5a2fde70ee05307af38c3f040c2c6951
kortsu <kortesoja.iivari@gmail.com>
2026-04-05 01:01:25 +0300

    Update portfolio-changer.js

--- SKRIPTIT/portfolio-changer.js
+++ SKRIPTIT/portfolio-changer.js
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
             title: "Combat Camera",
             media: [
                 { src: "/MEDIA/VALOKUVAUS/Valokuvaus13.webp", alt: "Combat 13" },
-                { src: "/MEDIA/VALOKUVAUS/Valokuvaus3.webp", alt: "Combat 23" },
-                { src: "/MEDIA/VALOKUVAUS/Valokuvaus14.webp", alt: "Teatterikuvaus 14" },
+                { src: "/MEDIA/VALOKUVAUS/Valokuvaus3.webp", alt: "Combat 3" },
+                { src: "/MEDIA/VALOKUVAUS/Valokuvaus14.webp", alt: "Combat 14" },
                 
                 { src: "/MEDIA/VALOKUVAUS/Valokuvaus1.webp", alt: "Combat 1" },
                 { src: "/MEDIA/VALOKUVAUS/Valokuvaus5.webp", alt: "Combat 5" },
